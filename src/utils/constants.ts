e93ac777717dc2f2fddd0f3fec6fb8d78a1d8b75
yebology <yobelnathaniel12@gmail.com>
2024-08-19 14:19:43 +0700

    delete redundancy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,6 @@ import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
 export const commitmentLevel = "confirmed";
 export const endpoint = clusterApiUrl("devnet");
-export const programId = import.meta.env.PROGRAM_ID;
 export const connection = new Connection(endpoint, commitmentLevel);
 
 export const survzProgramId = new PublicKey(idl.metadata.address);
